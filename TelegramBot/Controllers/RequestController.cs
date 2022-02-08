f309c14e992d0a8f0b3a1fe1fa5f04317b3db784
Daniil <daniilafendulov@bk.ru>
2022-02-08 22:23:01 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot/Controllers/RequestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramBot.Controllers
+{
+    [Route("TelegramBot")]
+    public class RequestController : Controller
+    {
+        private TelegramBotClient _client;
+        public RequestController(TelegramBotClient client)
+        {
+            _client = client;
+        }
+
+        [Route("AddRequest")]
+        public async Task<IActionResult> AddRequest()
+        {
+            // TODO: AddRequest
+            return Ok();
+        }
+    }
+}
---
+++ TelegramBot/TelegramBotServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    public static class TelegramBotServiceExtension
+    {
+        public static void AddTelegramBot(this IServiceCollection services, string token)
+        {
+            var bot = new TelegramBotClient(token);
+            //if (!bot.TestApiAsync().Result)
+            //{
+            //    throw new ArgumentException("неверный токен для Telegram бота");
+            //}
+            services.AddSingleton(bot);
+        }
+    }
+}
